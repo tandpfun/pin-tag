8140791addf46d21b5001730905c8be368c2d7f9
Thijs <hi@thijs.gg>
2024-04-07 11:33:48 -0700

    feat: protect admin routes

--- src/app/api/auth/create/route.ts
+++ src/app/api/auth/create/route.ts
@@ -2,6 +2,11 @@ import { createUser, generateAuthToken } from '@/lib/auth/database';
 import { PrismaClient, User } from '@prisma/client';
 
 export async function POST(request: Request) {
+  // Check for API token
+  const token = request.headers.get('Authorization');
+  if (!token || token !== `Bearer ${process.env.API_TOKEN}`)
+    return Response.json({ error: 'Unauthorized' }, { status: 401 });
+
   const body = await request.json().catch((_) => null);
   if (!body) return Response.json({ error: 'Invalid body' }, { status: 400 });
 
--- src/app/api/auth/login-as/route.ts
+++ src/app/api/auth/login-as/route.ts
@@ -3,6 +3,11 @@ import { PrismaClient } from '@prisma/client';
 import prisma from '../../../../lib/prisma';
 
 export async function GET(request: Request) {
+  // Check for API token
+  const token = request.headers.get('Authorization');
+  if (!token || token !== `Bearer ${process.env.API_TOKEN}`)
+    return Response.json({ error: 'Unauthorized' }, { status: 401 });
+
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('id');
 
